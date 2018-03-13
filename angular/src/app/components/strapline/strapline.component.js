14dde7c4247203fa4458e76f0c6b49f2ab1164bc
Arman47 <arman.hovhannisyan07@gmail.com>
2018-03-13 18:57:49 +0400

    angular test

--- angular/src/app/components/strapline/strapline.component.js
+++ angular/src/app/components/strapline/strapline.component.js
@@ -1,15 +1,38 @@
 'use strict';
 
 class Strapline {
-	constructor(){
-		this.line = `Our average time from pickup to drop-off`
+	constructor($interval){
+		this.$interval = $interval
+		this.line = 'Our average time from pickup to drop-off'
+		this.lines = [
+			"Our average time from pickup to drop-off",
+			"Quicker than your commute",
+			"So fast it'll blow your socks off",
+			'"Slow and steady wins the race" - not someone from Repairly',
+			'Recently compared to the Hadron Collider',
+			'Repairly representative spotted in Bugatti Veyron, Feb 2016',
+			'Usain Bolt, team leader at Repairly'
+		]
 	}
+
+	$onInit() {
+		this.$interval(() => {
+			this.updateLine();
+		}, 1000)
+	}
+
+	updateLine(){
+		let lines_length = this.lines.length
+		let custom_index = parseInt(Math.random() * lines_length)
+
+		this.line = this.lines[custom_index]
+	}
+
 }
 
-Strapline.$inject = [];
+Strapline.$inject = ['$interval'];
 
 export const strapline = {
-  controller: Strapline,
-  templateUrl: require('ngtemplate!./strapline.html')
+	controller: Strapline,
+	templateUrl: require('ngtemplate!./strapline.html')
 }
-  
